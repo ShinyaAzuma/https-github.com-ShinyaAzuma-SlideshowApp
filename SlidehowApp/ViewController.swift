571bd7be200767e478ad333f8723a13da4dcd0e9
東真也 <azuma0526@yahoo.co.jp>
2019-04-28 18:26:49 +0900

    Initial Commit

---
+++ SlidehowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlidehowApp
+//
+//  Created by 東真也 on 2019/04/28.
+//  Copyright © 2019 NEXTSTAGE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
