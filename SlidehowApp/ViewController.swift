11f67e542ef0c6e915961ed349bce453d0b3d5a2
東真也 <azuma0526@yahoo.co.jp>
2019-04-29 18:15:08 +0900

    再提出

--- SlidehowApp/ViewController.swift
+++ SlidehowApp/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var slideImageView: UIImageView!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var preButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    
     var timer: Timer!
     let slideImages:[String] = ["1.jpg","2.jpg","3.jpg"]
     var index: Int = 0
@@ -42,21 +44,24 @@ class ViewController: UIViewController {
         }
     }
     
-    
     @IBAction func startButton(_ sender: Any) {
         if self.timer == nil {
             self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateImage), userInfo: nil, repeats: true)
             // 進む・戻るボタンの無効化
             preButton.isEnabled = false
             nextButton.isEnabled = false
+            startButton.setTitle("停止", for: .normal)
         }else{
             self.timer.invalidate()   // 現在のタイマーを破棄する
             self.timer = nil          // startTimer() の timer == nil で判断するために、 timer = nil としておく
             // 進む・戻るボタンの有効化
             preButton.isEnabled = true
             nextButton.isEnabled = true
+            startButton.setTitle("再生", for: .normal)
         }
     }
+    
+   
     
     @IBAction func unwind(_ segue: UIStoryboardSegue){
         
